58158b306ace3721a64b9bfd1fabcd1393dc48f0
Danil Pismenny <danil@orionet.ru>
2013-06-22 13:27:05 +0400

    anon login

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module Masha
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.precompile += %w( admin.css )
+    config.assets.precompile += %w( admin.css *.png *.jpg *.jpeg *.gif)
   end
 end
