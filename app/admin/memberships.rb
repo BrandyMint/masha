247e2c684c567e90570791da1a14c684a67f5208
Danil Pismenny <danil@orionet.ru>
2013-06-25 16:30:07 +0400

    edit roles in aa

--- app/admin/memberships.rb
+++ app/admin/memberships.rb
@@ -1,3 +1,9 @@
 ActiveAdmin.register Membership do
+  index do
+    column :project, :sortable => :project_id
+    column :user, :sortable => :user_id
+    column :role, :sortable => :role_cd
+    actions
+  end
 
 end
