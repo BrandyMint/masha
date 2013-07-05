18f8e1958875dbbfceb43fdb094fdfbfd2cc1a52
heretge <vozdelaluz@gmail.com>
2013-07-05 15:24:06 +0400

    time_shift date validation

--- app/models/time_shift.rb
+++ app/models/time_shift.rb
@@ -6,7 +6,7 @@ class TimeShift < ActiveRecord::Base
 
   validates :project, :presence => true
   validates :user, :presence => true
-  validates :date, :presence => true
+  validates_date :date, :on_or_before => lambda { Date.today }
   validates :hours, :presence => true, :numericality => { :greater_than_or_equal_to => 0.1, :less_than_or_equal_to => 24 }
   validates :description, :presence => true, :uniqueness => { :scope => [:project_id, :user_id, :date, :hours] }
 end
