b1f959cd3ac187fb373f9646611db52046008985
Danil Pismenny <danil@orionet.ru>
2013-10-23 12:13:51 +0400

    [#94] Feedback page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+class PagesController < ApplicationController
+  def feedback
+
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Masha::Application.routes.draw do
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
+  get "feedback" => "pages#feedback", :as => "feedback"
+
   resources :users, :only => [:new, :create]
   resources :sessions, :only => [:new, :create, :destroy]
 
