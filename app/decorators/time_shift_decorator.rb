8141a181268401126e1df1521cb1a445d6b47336
Danil Pismenny <danil@orionet.ru>
2013-10-23 17:50:30 +0400

    Вынес кнопку удалить на страницу редактирования time_shift-а

--- app/decorators/time_shift_decorator.rb
+++ app/decorators/time_shift_decorator.rb
@@ -31,9 +31,9 @@ class TimeShiftDecorator < Draper::Decorator
 
   def remove_link
     if h.current_user.can_delete?(source)
-      h.link_to h.time_shift_path(source), :data => { method: :delete, confirm: I18n.t('time_shifts.delete.confirm') } do
-        h.ficon 'cancel-1', color: :gray, size: 18
-      end
+      h.link_to 'Удалить',
+        h.time_shift_path(source),
+        :class => "btn btn-danger btn-small", :data => { method: :delete, confirm: I18n.t('time_shifts.delete.confirm') }
     end
   end
 
