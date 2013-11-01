8242ff329648cb81f81df05bcd0a3c8c10d9fa49
Danil Pismenny <danil@orionet.ru>
2013-11-01 16:58:47 +0400

    Исправил доступность инвайтов для удаления

--- app/authorizers/invite_authorizer.rb
+++ app/authorizers/invite_authorizer.rb
@@ -1,2 +1,5 @@
-class InviteAuthorizer < MembershipAuthorizer
+class InviteAuthorizer < ApplicationAuthorizer
+  def deletable_by? user
+    user.is_root? || user.has_role?(:owner, resource.project)
+  end
 end
--- app/decorators/invite_decorator.rb
+++ app/decorators/invite_decorator.rb
@@ -2,9 +2,10 @@ class InviteDecorator < Draper::Decorator
   delegate_all
 
   def remove_link
+    binding.pry
     if h.current_user.can_delete?(source)
       h.link_to h.invite_path(source), :data => { method: :delete, confirm: I18n.t('time_shifts.delete.confirm') } do
-        h.ficon 'cancel-1'
+        h.ficon 'cancel-1', color: :gray, size: 18
       end
     end
   end
