3cdcc4425e486816b1275a0aefe105bc83646266
Danil Pismenny <danil@orionet.ru>
2013-11-14 15:23:56 +0400

    minor

--- app/models/time_shift.rb
+++ app/models/time_shift.rb
@@ -12,6 +12,6 @@ class TimeShift < ActiveRecord::Base
   validates :project, :presence => true
   validates :user, :presence => true
   validates :date, :timeliness => { :on_or_before => lambda { Date.today }, :type => :date }
-  validates :hours, :presence => true, :numericality => { :greater_than_or_equal_to => 0.1, :less_than_or_equal_to => 24 }
+  validates :hours, :presence => true, :numericality => { :greater_than_or_equal_to => 0.1 }
   validates :description, :presence => true, :uniqueness => { :scope => [:project_id, :user_id, :date, :hours] }
 end
