1fbc8c399b02eb8e6b36d7c932420a5b546e10fd
vadus1 <bekish94.94@mail.ru>
2014-09-23 03:11:18 +0300

    [#179] Кнопка изархива доступна не владельцам

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -13,15 +13,16 @@ module ProjectsHelper
   end
 
   def change_project_status_link project
-    if project.active
-      if project.time_shifts.any?
-        link_to t('project.archivate'), archivate_project_path(@project), :method=>:put, :class => 'btn btn-default btn-mini'
+    if current_user.can_create?(Membership.new(project: project))
+      if project.active
+        if project.time_shifts.any?
+          link_to t('project.archivate'), archivate_project_path(@project), method: :put, class: 'btn btn-default btn-mini'
+        else
+          link_to t('project.remove'), project_path(@project), method: :delete, class: 'btn btn-danger btn-mini'
+        end
       else
-        link_to t('project.remove'), project_path(@project), :method=>:delete, :class => 'btn btn-danger btn-mini'
+        link_to t('project.activate'), activate_project_path(@project), method: :put, class: 'btn btn-default btn-mini'
       end
-    else
-      link_to t('project.activate'), activate_project_path(@project), :method=>:put, :class => 'btn btn-default btn-mini'
     end
   end
-
 end
