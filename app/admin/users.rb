f2f71192fa0fa1dc1a1e3b11caeb67c1979b5208
Danil Pismenny <danil@orionet.ru>
2014-10-16 15:25:49 +0400

    fix admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register User do
   index do
     column :id
     column :avatar do |user|
-      UserDecorator.decorate(user).avatar
+      UserDecorator.decorate(user).avatar rescue 'err'
     end
     column :created_at
     column :name
@@ -13,21 +13,29 @@ ActiveAdmin.register User do
     column :email
     column :is_root
     column :memberships do |user|
-      ul do
-        user.memberships.includes(:project).each do |m|
-          li do
-            link_to "#{m.project} (#{m.role})", admin_membership_url(m.id)
+      begin
+        ul do
+          user.memberships.includes(:project).each do |m|
+            li do
+              link_to "#{m.project} (#{m.role})", admin_membership_url(m.id)
+            end
           end
         end
+      rescue => err
+        err
       end
     end
     column :authentications do |user|
-      ul do
-        user.authentications.each do |a|
-          li do
-            link_to "#{a.provider} (#{a.nickname}) <#{a.email}>", admin_authentications_url(:q => { :user_id_in=>user.id })
+      begin
+        ul do
+          user.authentications.each do |a|
+            li do
+              link_to "#{a.provider} (#{a.nickname}) <#{a.email}>", admin_authentications_url(:q => { :user_id_in=>user.id })
+            end
           end
         end
+      rescue => err
+        err
       end
     end
     actions
