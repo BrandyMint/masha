accfa1a448472475d2f9d6251571404db13fc29a
Freebit <freebit77@inbox.com>
2014-12-16 09:47:33 +0300

    fix date format and invite model spec

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Invite do
 
-  let(:invite) { create :invite}
+  let(:invite) {create :invite}
 
   it 'Если мы созадем инвайт с уже существуюим емайлом в другом проекте, мы не получаем ошибок' do
-    expect{ create :invite, email: invite.email + "wer" }.to_not raise_error
+    expect{ create :invite, email :invite.email }.to_not raise_error
   end
 
 end
