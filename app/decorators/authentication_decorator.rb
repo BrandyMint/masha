ee6cb9ae652028773e25597b13f99cadc38362f7
Freebit <freebit77@inbox.com>
2014-12-25 13:11:26 +0300

    fix profile user link

--- app/decorators/authentication_decorator.rb
+++ app/decorators/authentication_decorator.rb
@@ -2,6 +2,6 @@ class AuthenticationDecorator < ApplicationDecorator
   delegate_all
 
   def nickname
-    h.link_to source.nickname, source.url
+    h.link_to source.nickname, source.html_url, html_options = {target: source.html_url.present? ? "_blank" : "_self"}
   end
 end
