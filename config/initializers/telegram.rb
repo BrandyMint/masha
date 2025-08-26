b1662799ddccbf0d1cdf4bef6dfa7e167486bd94
Danil Pismenny <danil@brandymint.ru>
2025-08-26 23:50:47 +0300

    Remove autologger

--- config/initializers/telegram.rb
+++ config/initializers/telegram.rb
@@ -1,9 +1,5 @@
 # frozen_string_literal: true
 
-Rails.application.config.telegram_updates_controller.logger =
-  ActiveSupport::Logger.new(Rails.root.join('log/telegram.log'))
-                       .tap { |logger| logger.formatter = AutoLogger::Formatter.new }
-
 Rails.application.config.telegram_updates_controller.session_store = :redis_cache_store,
                                                                      { url: ApplicationConfig.redis_cache_store_url,
                                                                        expires_in: 1.month }
