c345f0bd343c4cce10eb92dd0827cbd1c06cdb4a
Danil Pismenny <danil@brandymint.ru>
2025-08-26 23:51:17 +0300

    Fix bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Bugsnag.configure do |config|
-  config.api_key = Rails.application.credentials.bugsnag_api_key
   config.app_version = AppVersion.format('%M.%m.%p')
   config.notify_release_stages = %w[production staging]
 end
