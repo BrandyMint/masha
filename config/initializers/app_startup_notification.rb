8ceccf243e920ca7a42e081cb744cddfa2b6a905
Danil Pismenny <danil@brandymint.ru>
2025-09-17 17:52:36 +0300

    Fix NotifiedVersion

--- config/initializers/app_startup_notification.rb
+++ config/initializers/app_startup_notification.rb
@@ -3,6 +3,6 @@
 unless ENV.key? 'SECRET_KEY_BASE_DUMMY'
   # Send startup notification to developer in production
   Rails.application.config.after_initialize do
-    NotifiedVersion.create version: AppVersion.to_s if Rails.env.production?
+    NotifiedVersion.create version: AppVersion.to_s if Rails.env.production? && NotifiedVersion.table_exists?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_05_043820) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_15_130100) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -79,6 +79,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_05_043820) do
     t.index ["project_id"], name: "index_memberships_on_project_id"
     t.index ["user_id", "project_id"], name: "index_memberships_on_user_id_and_project_id", unique: true
     t.index ["user_id"], name: "index_memberships_on_user_id"
+  end
+
+  create_table "notified_versions", force: :cascade do |t|
+    t.string "version", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.index ["version"], name: "index_notified_versions_on_version", unique: true
   end
 
   create_table "projects", force: :cascade do |t|
