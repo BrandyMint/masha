8878bd0f35907a4ac2c4c94ba3b5e77aa8484213
Danil Pismenny <danil@brandymint.ru>
2025-09-21 15:25:23 +0300

    MIgrate from bootstrap 3 to bootstrap 5

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -1,52 +1,88 @@
 # frozen_string_literal: true
 
 # Use this setup block to configure all options available in SimpleForm.
-# Inside your views, use the 'simple_form_for' with one of the Bootstrap form
-# classes, '.form-horizontal', '.form-inline', '.form-search' or
-#  '.form-vertical', as the following:
-#
-# = simple_form_for(@user, html: {class: 'form-horizontal' }) do |form|'
+# Updated for Bootstrap 5 compatibility
+# Inside your views, use 'simple_form_for' with Bootstrap 5 form classes
 SimpleForm.setup do |config|
-  config.wrappers :bootstrap, tag: 'div', class: 'control-group', error_class: 'error' do |b|
+  # Bootstrap 5 default wrapper
+  config.wrappers :bootstrap, tag: 'div', class: 'mb-3', error_class: 'is-invalid' do |b|
     b.use :html5
     b.use :placeholder
-    b.use :label
-    b.wrapper tag: 'div', class: 'controls' do |ba|
-      ba.use :input
-      ba.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
-      ba.use :hint,  wrap_with: { tag: 'p', class: 'help-block' }
-    end
+    b.use :label, class: 'form-label'
+    b.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
+    b.use :error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+    b.use :hint, wrap_with: { tag: 'div', class: 'form-text' }
   end
 
-  config.wrappers :prepend, tag: 'div', class: 'control-group', error_class: 'error' do |b|
+  # Bootstrap 5 floating labels wrapper
+  config.wrappers :floating, tag: 'div', class: 'form-floating mb-3', error_class: 'is-invalid' do |b|
     b.use :html5
     b.use :placeholder
-    b.use :label
-    b.wrapper tag: 'div', class: 'controls' do |input|
-      input.wrapper tag: 'div', class: 'input-prepend' do |prepend|
-        prepend.use :input
-      end
-      input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
-      input.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
-    end
+    b.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
+    b.use :label, class: 'form-label'
+    b.use :error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+    b.use :hint, wrap_with: { tag: 'div', class: 'form-text' }
   end
 
-  config.wrappers :append, tag: 'div', class: 'control-group', error_class: 'error' do |b|
+  # Bootstrap 5 input group wrapper for prepend/append
+  config.wrappers :input_group, tag: 'div', class: 'mb-3', error_class: 'is-invalid' do |b|
     b.use :html5
     b.use :placeholder
-    b.use :label
-    b.wrapper tag: 'div', class: 'controls' do |input|
-      input.wrapper tag: 'div', class: 'input-append' do |append|
-        append.use :input
-      end
-      input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
-      input.use :error, wrap_with: { tag: 'span', class: 'help-inline' }
+    b.use :label, class: 'form-label'
+    b.wrapper tag: 'div', class: 'input-group' do |input_group|
+      input_group.use :input, class: 'form-control', error_class: 'is-invalid', valid_class: 'is-valid'
     end
+    b.use :error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+    b.use :hint, wrap_with: { tag: 'div', class: 'form-text' }
   end
 
-  # Wrappers for forms and inputs using the Twitter Bootstrap toolkit.
-  # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
-  # to learn about the different styles for forms and inputs,
-  # buttons and other elements.
+  # Bootstrap 5 checkbox wrapper
+  config.wrappers :check_boxes, tag: 'div', class: 'mb-3', error_class: 'is-invalid' do |b|
+    b.use :html5
+    b.wrapper tag: 'div', class: 'form-check' do |ba|
+      ba.use :input, class: 'form-check-input', error_class: 'is-invalid', valid_class: 'is-valid'
+      ba.use :label, class: 'form-check-label'
+    end
+    b.use :error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+    b.use :hint, wrap_with: { tag: 'div', class: 'form-text' }
+  end
+
+  # Bootstrap 5 radio buttons wrapper
+  config.wrappers :radio_buttons, tag: 'div', class: 'mb-3', error_class: 'is-invalid' do |b|
+    b.use :html5
+    b.wrapper tag: 'div', class: 'form-check' do |ba|
+      ba.use :input, class: 'form-check-input', error_class: 'is-invalid', valid_class: 'is-valid'
+      ba.use :label, class: 'form-check-label'
+    end
+    b.use :error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+    b.use :hint, wrap_with: { tag: 'div', class: 'form-text' }
+  end
+
+  # Bootstrap 5 select wrapper
+  config.wrappers :select, tag: 'div', class: 'mb-3', error_class: 'is-invalid' do |b|
+    b.use :html5
+    b.use :placeholder
+    b.use :label, class: 'form-label'
+    b.use :input, class: 'form-select', error_class: 'is-invalid', valid_class: 'is-valid'
+    b.use :error, wrap_with: { tag: 'div', class: 'invalid-feedback' }
+    b.use :hint, wrap_with: { tag: 'div', class: 'form-text' }
+  end
+
+  # Set default wrapper
   config.default_wrapper = :bootstrap
+
+  # Configure button class
+  config.button_class = 'btn btn-primary'
+
+  # Configure boolean style
+  config.boolean_style = :nested
+
+  # Configure collection wrapper class
+  config.collection_wrapper_class = 'mb-3'
+
+  # Configure item wrapper class
+  config.item_wrapper_class = 'form-check'
+
+  # Configure label text for boolean inputs
+  config.boolean_label_class = 'form-check-label'
 end
