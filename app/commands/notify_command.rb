50efaec2c74900049141bd3acbf45bc7cf1949aa
Danil Pismenny <dapi@users.noreply.github.com>
2025-11-12 11:30:49 +0300

    Minor refactor commands

--- app/commands/notify_command.rb
+++ app/commands/notify_command.rb
@@ -14,34 +14,14 @@ class NotifyCommand < BaseCommand
     respond_with :message, text: t('commands.notify.prompts.enter_message')
   end
 
-  def notify_message_input(*)
-    # Получаем полный текст сообщения из payload
-    message_text = payload['text']
-
-    # Проверка отмены
+  def notify_message_input(message_text = '')
+    return respond_with :message, text: t('commands.notify.errors.empty_message') if message.blank?
+    return respond_with :message, text: t('commands.notify.errors.too_short') if message.length < MIN_MESSAGE_LENGTH
+    return respond_with :message, text: t('commands.notify.errors.too_long') if message.length > MAX_MESSAGE_LENGTH
     return respond_with :message, text: t('commands.notify.cancelled') if message_text.downcase.strip == 'cancel'
 
-    validation_result = validate_message(message_text)
-    return validation_result unless validation_result == true
-
-    recipients = fetch_recipients
+    recipients = TelegramUser.all
     BroadcastNotificationJob.perform_later(message_text, recipients.map(&:id))
     respond_with :message, text: t('commands.notify.success', count: recipients.count)
-  end
-
-  private
-
-  def validate_message(message)
-    return respond_with :message, text: t('commands.notify.errors.empty_message') if message.blank?
-
-    return respond_with :message, text: t('commands.notify.errors.too_short') if message.length < MIN_MESSAGE_LENGTH
-
-    return respond_with :message, text: t('commands.notify.errors.too_long') if message.length > MAX_MESSAGE_LENGTH
-
-    true
-  end
-
-  def fetch_recipients
-    TelegramUser.all
   end
 end
