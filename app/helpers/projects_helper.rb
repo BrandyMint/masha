3813e687f934dd4b28d5d02bbbc64f7076393d40
Danil Pismenny <dapi@users.noreply.github.com>
2025-11-16 20:44:14 +0300

    Rubocop lint

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -14,13 +14,13 @@ module ProjectsHelper
 
     if project.active
       if project.time_shifts.any?
-        link_to t('project.archivate'), archivate_project_path(@project), method: :put,
-                                                                          class: 'btn btn-default btn-mini'
+        link_to t('project.archivate'), archivate_project_path(project), method: :put,
+                                                                         class: 'btn btn-default btn-mini'
       else
-        link_to t('project.remove'), project_path(@project), method: :delete, class: 'btn btn-danger btn-mini'
+        link_to t('project.remove'), project_path(project), method: :delete, class: 'btn btn-danger btn-mini'
       end
     else
-      link_to t('project.activate'), activate_project_path(@project), method: :put, class: 'btn btn-default btn-mini'
+      link_to t('project.activate'), activate_project_path(project), method: :put, class: 'btn btn-default btn-mini'
     end
   end
 end
